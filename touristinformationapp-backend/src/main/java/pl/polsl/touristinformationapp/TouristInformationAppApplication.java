26daae7c69f31abb291cfbb15b979a49ddeaa311
PKozdra <piotr@kozdra.pl>
2022-04-26 15:33:26 +0200

    initial commit

---
+++ touristinformationapp-backend/src/main/java/pl/polsl/touristinformationapp/TouristInformationAppApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.touristinformationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristInformationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristInformationAppApplication.class, args);
+    }
+
+}
---
+++ touristinformationapp-backend/src/test/java/pl/polsl/touristinformationapp/TouristInformationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.touristinformationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristInformationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

5e4586000e606e934ef0c22012d2cc2e6f0b3c5e
PKozdra <piotr@kozdra.pl>
2022-04-26 20:54:07 +0200

    added navigation bar

--- touristinformationapp-frontend/src/App.tsx
+++ touristinformationapp-frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
 import './App.css';
-import Login from './Login';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+
 
 
 function App() {
+
   return (
-    <div className="App">
-     <Login />
-    </div>
+  <>
+  <Navbar/>
+  <Login/>
+  </>
   );
 }
 

ddef476dc8957c90ba4443474ea2f5686628b27e
BartMat <matusikowy@gmail.com>
2022-05-10 20:56:14 +0200

    fixed "zaloguj się" button

--- touristinformationapp-frontend/src/components/Navbar.tsx
+++ touristinformationapp-frontend/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   //const content = <React.Fragment>Hey, I'm a model.</React.Fragment>; 
 
   return (
-    <nav className="bg-gray-800 border-gray-200 px-2 sm:px-4 py-2.5 rounded bg-gray-800">
+    <nav className="bg-gray-800 border-gray-200 px-2 sm:px-4 py-2.5 rounded bg-gray-00">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <a href="#" className="flex items-center">
           <img src={logo} className={styles.logo} alt="Logo Polsl" />
@@ -37,7 +37,7 @@ const Navbar = () => {
             <button
               onClick={handleLogin}
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 :bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
             >
               Zaloguj się
             </button>

ab65149f2fafd7e53c866c8c2b91c12753da9651
xoamJdAywKwBexhKC8os <wiktor.wadas@itsilesia.com>
2022-05-10 20:03:22 +0200

    added blog in navibar

--- touristinformationapp-frontend/src/components/Navbar.tsx
+++ touristinformationapp-frontend/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import logo from "../images/Logo_PolSl.png";
 import styles from "./Navbar.module.css";
 import { Modal } from "../modal/modal";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import useModal from "../modal/useModal";
 import Login from "./Login";
 import Register from "./Register";
@@ -117,14 +118,7 @@ const Navbar = () => {
                 O nas
               </a>
             </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Kontakt
-              </a>
-            </li>
+            <Link to="/blog">Blog</Link>
           </ul>
         </div>
       </div>
